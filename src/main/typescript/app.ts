8adfb9d9af6536ffd4112822fd155296c796783d
TaeHyun Kim <galcyurio@gmail.com>
2021-08-25 02:39:04 +0900

    JSDoc 추가

--- src/main/typescript/app.ts
+++ src/main/typescript/app.ts
@@ -1,5 +1,9 @@
 import { Survey, SurveyStep, newSurveyStep, SurveyResult } from './survey';
 
+/**
+ * @param answers {string[]} 답변 목록
+ * @returns 설문 결과
+ */
 export function getSurveyResult(answers: string[]): string {
   let survey: Survey = newSurveyStep(0)
   while (survey instanceof SurveyStep) {
